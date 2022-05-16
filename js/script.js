bd6b8b7bbd9e1a8e2b96932fa58b751754b26a4b
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-05-16 20:20:13 +0430

    👔 Added getRandomNumber function and displayQuote

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 import config from "./config.js";
 
+const btn = document.getElementById("btnQuote");
+const getRandomNumber = function (max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 const getRandomQuote = async function () {
   const data = await config.createRequest("api/quotes", "GET");
   const quotes = await data.json();
+  const randomNumber = getRandomNumber(quotes.length, 0);
+  return quotes[randomNumber];
 };
-export default getRandomQuote;
+
+const displayQuote = function () {
+  const quote = getRandomQuote();
+};
+
+btn.addEventListener("click", displayQuote);
