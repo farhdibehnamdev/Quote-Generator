4bfba7482d171749ce584708703928d4e41e64ac
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-05-17 12:52:53 +0430

    👔 Added "init()" function to get the first quote.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 import config from "./config.js";
 
-const btn = document.getElementById("btnQuote");
+const btn = document.querySelector(".btnQuote");
+const h1Element = document.querySelector(".quote-text");
+const authorEl = document.querySelector(".author-name");
+function init() {
+  getRandomQuote();
+  displayQuote();
+}
 const getRandomNumber = function (max, min) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -12,8 +18,10 @@ const getRandomQuote = async function () {
   return quotes[randomNumber];
 };
 
-const displayQuote = function () {
-  const quote = getRandomQuote();
+const displayQuote = async function () {
+  const quote = await getRandomQuote();
+  h1Element.textContent = quote.text;
+  authorEl.textContent = quote.author;
 };
-
+init();
 btn.addEventListener("click", displayQuote);
