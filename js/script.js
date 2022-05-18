e2261059edbc755bae928d65d61fbb85b16669e4
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-05-18 11:39:00 +0430

    🔥 Removed getRandomNumber.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
 import config from "./config.js";
 
 const btn = document.querySelector(".btnQuote");
-const h1Element = document.querySelector(".quote-text");
+const quoteElement = document.querySelector(".quote-text");
 const authorEl = document.querySelector(".author-name");
 const spinnerEl = document.querySelector(".loader");
+const innerContainerEl = document.querySelector(".inner-container");
+const twitterBtn = document.querySelector(".twitter");
 function init() {
-  getRandomQuote();
+  getQuote();
   displayQuote();
 }
-const getRandomNumber = function (max, min) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+
 /// Loader
-// const loader = function () {
-//   spinnerEl.classList.
-// };
-const getRandomQuote = async function () {
-  const data = await config.createRequest("api/quotes", "GET");
-  const quotes = await data.json();
-  const randomNumber = getRandomNumber(quotes.length, 0);
-  return quotes[randomNumber];
+const loader = function (quote) {
+  if (quote) {
+    spinnerEl.classList.remove("hidden");
+    innerContainerEl.classList.add("hidden");
+  } else {
+    spinnerEl.classList.add("hidden");
+    innerContainerEl.classList.remove("hidden");
+  }
+};
+const getQuote = async function () {
+  const data = await config.createRequest(
+    "api/1.0/?method=getQuote&lang=en&format=json",
+    "GET"
+  );
+  const quote = await data.json();
+  return quote;
 };
 
 const displayQuote = async function () {
-  const quote = await getRandomQuote();
-  h1Element.textContent = quote.text;
-  authorEl.textContent = `"${quote.author}"`;
+  const quote = await getQuote();
+  if (!quote.quoteText) {
+    loader(true);
+  } else {
+    loader(false);
+  }
+  quoteElement.textContent = quote.quoteText;
+  authorEl.textContent = `"${quote.quoteAuthor}"`;
 };
+
+const sendTweet = function () {
+  const quote = quoteElement.textContent;
+  const author = authorEl.textContent;
+  let twitterUrl = `href=https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+};
+
 init();
 btn.addEventListener("click", displayQuote);
+twitterBtn.addEventListener("click", sendTweet);
